b240a2b8acf0dc27c4ab71a22ff13f616055e9d5
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-02-15 10:31:40 -0500

    passed all tests for PersonalClasses

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,85 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String hairColor;
+    private double height;
+    private boolean wearsGlasses;
+    private String eyeColor;
+    private boolean isWeird;
 
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = "Leon";
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = "Leon";
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = "Blue";
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = "Brown";
     }
 
     public void setAge(int age) {
+        this.age = 5;
+    }
+
+    public void setHeight(double height) {
+        this.height = 2.0;
+    }
+
+    public void setIsWeird(boolean weird) {
+        isWeird = weird;
+    }
+
+    public void setWearsGlasses(boolean blind) {
+        wearsGlasses = blind;
     }
 
     public String getName() {
-        return null;
+        return this.name;
+    }
+
+    public String getEyeColor() {
+        return this.eyeColor;
+    }
+
+    public String getHairColor() {
+        return this.hairColor;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
+
+    public Double getHeight() {
+        return this.height;
+    }
+
+    public Boolean getIsWeird() {
+        return this.isWeird;
+    }
+
+    public Boolean getWearsGlasses() {
+        return this.wearsGlasses;
+    }
+
+
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -95,4 +95,77 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testSetEyeColor() {
+        // Given
+        Person person = new Person();
+        String expected = "Blue";
+
+        // When
+        person.setEyeColor(expected);
+        String actual = person.getEyeColor();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetHairColor() {
+        // Given
+        Person person = new Person();
+        String expected = "Brown";
+
+        // When
+        person.setHairColor(expected);
+        String actual = person.getHairColor();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetHeight() {
+        // Given
+        Person person = new Person();
+        double expected = 2.0;
+
+        // When
+        person.setHeight(expected);
+
+        // Then
+        double actual = person.getHeight();
+        Assert.assertEquals( expected, actual, 0.01);
+    }
+
+    @Test
+    public void testIsWeird() {
+        // Given
+        Person person = new Person();
+        boolean expected = true;
+
+        // When
+        person.setIsWeird(expected);
+
+        // Then
+        boolean actual = person.getIsWeird();
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void testWearsGlasses() {
+        // Given
+        Person person = new Person();
+        boolean expected = true;
+
+        // When
+        person.setWearsGlasses(expected);
+
+        // Then
+        boolean actual = person.getWearsGlasses();
+        Assert.assertTrue(actual);
+    }
+
+
+
 }
